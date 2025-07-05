e7855122feb263edde74615e943de59efb720097
nhardwajvivekkumar <vk3411389@gmail.com>
2025-07-05 20:17:48 +0530

    Some updated changes

--- src/components/ utils.js
+++ src/components/ utils.js
@@ -1,8 +1,10 @@
 export const getNotes = () => {
-    const notes = JSON.parse(localStorage.getItem('notes')) || [];
-    return notes;
-  };
-  
-  export const saveNotes = (notes) => {
-    localStorage.setItem('notes', JSON.stringify(notes));
-  };
+  const notes = JSON.parse(localStorage.getItem("notes")) || [];
+  return notes;
+};
+
+export const saveNotes = (notes) => {
+  localStorage.setItem("notes", JSON.stringify(notes));
+};
+
+// i do this change in utils component to learn how to do git push
